5760acc8e7bcbf8b442e52b896ebfc9d80c4e695
rajesh-itgeeks <rajeshchoudhary@itgeeks.com>
2023-12-01 18:46:32 +0530

    chnages

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,17 +1,24 @@
 const Sequelize = require('sequelize');
-//const Store = require('../model/userModel')
-//const Title = require('../model/productModel')
-const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-  host: process.env.POSTGRES_HOST,
-  dialect: 'postgres'
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
+
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD, {
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+  }
+);
 
 try {
   sequelize.authenticate();
   console.log('Connection has been established successfully.');
-  sequelize.sync()
-  //await sequelize.sync();
-  console.log("create table")
+  sequelize.sync();
+  console.log("Table created");
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 }
